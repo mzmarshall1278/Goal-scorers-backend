938fd08170a8407b286023a8d997346c23357a61
Marshall <mzmarshall1278@gmail.com>
2021-07-08 11:44:34 +0100

    Add and connected to postgres with typorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GoalsModule } from './goals/goals.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [GoalsModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    GoalsModule,
+  ],
 })
 export class AppModule {}

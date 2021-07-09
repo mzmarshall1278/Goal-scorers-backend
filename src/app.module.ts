cfec7205c69555fe369474198229ac78f8c40102
Marshall <mzmarshall1278@gmail.com>
2021-07-09 09:44:04 +0100

    Initialized Auth module with service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { GoalsModule } from './goals/goals.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     GoalsModule,
+    AuthModule,
   ],
 })
 export class AppModule {}

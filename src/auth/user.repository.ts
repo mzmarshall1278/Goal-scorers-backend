98edc8717819a10e20c0f697e62d157a34aff393
Marshall <mzmarshall1278@gmail.com>
2021-07-09 09:52:25 +0100

    created a user entity and user repository and injected into the service

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,7 @@
+import { Entity, EntityRepository, Repository } from "typeorm";
+import { User } from './user.entity';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User>{
+
+}
